77ac58bd6b8cf42a3a25440ee6c905de894bae07
Lennart Heinrich <lennart@ltheinrich.de>
2020-03-28 13:57:07 +0100

    add cpu usage logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+//! Monirs main
+#![cfg(target_os = "linux")]
+
+mod sys;
+
+use kern::Fail;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use sys::cpu_usage;
+
+fn main() -> Result<(), Fail> {
+    let mut log = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .append(true)
+        .open("log.txt")
+        .unwrap();
+    let (rc, _) = cpu_usage(Duration::from_secs(60));
+    loop {
+        let perc = rc.recv().unwrap();
+        let time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+        log.write_all(
+            format!(
+                "{:02}:{:02}:{:02} -> CPU usage ={:>6}%\n",
+                (time.as_secs() / (60 * 60)) % 24,
+                (time.as_secs() / 60) % 60,
+                time.as_secs() % 60,
+                format!("{:.2}", perc)
+            )
+            .as_bytes(),
+        )
+        .unwrap();
+    }
 }
---
+++ src/sys.rs
@@ -0,0 +1,84 @@
+//! System data collector
+
+use kern::Fail;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::thread::{sleep, spawn, JoinHandle};
+use std::time::Duration;
+
+/// Sends cpu usage via channel every duration (sends nothing during first duration)
+pub fn cpu_usage(duration: Duration) -> (Receiver<f64>, JoinHandle<Result<(), Fail>>) {
+    // create channel
+    let (tx, rc): (Sender<f64>, Receiver<f64>) = channel();
+
+    // spawn sender thread
+    let thread = spawn(move || {
+        // read first cpu times and wait first second
+        let (mut prev_idle, mut prev_total) = read_cpu_times()?;
+        sleep(duration);
+
+        // send cpu usage continously
+        loop {
+            // read cpu times and calculate difference
+            let (idle, total) = read_cpu_times()?;
+            let dif_idle = (idle - prev_idle) as f64;
+            let dif_total = (total - prev_total) as f64;
+
+            // calculate cpu usage and send
+            let cpu_usage = ((1.0 - dif_idle / dif_total) * 10000.0).round() / 100.0;
+            tx.send(cpu_usage).or_else(Fail::from)?;
+
+            // set previous times and wait next second
+            prev_idle = idle;
+            prev_total = total;
+            sleep(duration);
+        }
+    });
+
+    // return receiver and thread
+    (rc, thread)
+}
+
+// get cpu idle and total time from /proc/stat
+fn read_cpu_times() -> Result<(u64, u64), Fail> {
+    // open file
+    let mut file = OpenOptions::new()
+        .read(true)
+        .open("/proc/stat")
+        .or_else(Fail::from)?;
+
+    // read file
+    let mut buf = String::new();
+    file.read_to_string(&mut buf).or_else(Fail::from)?;
+
+    // only first line
+    buf = {
+        // split lines and get first
+        let line = buf
+            .split('\n')
+            .next()
+            .ok_or_else(|| Fail::new("broken /proc/stat"))?;
+
+        // line should be at least 10 characters
+        if line.len() < 10 {
+            return Fail::from("broken /proc/stat");
+        }
+
+        // cut "cpu  "
+        (&line[5..line.len()]).to_string()
+    };
+
+    // split by whitespace and get idle time
+    let split: Vec<&str> = buf.split_ascii_whitespace().collect();
+    let idle: u64 = split[3].parse().or_else(Fail::from)?;
+
+    // calculate total from all times
+    let mut total = 0u64;
+    for s in split {
+        total += s.parse::<u64>().or_else(Fail::from)?;
+    }
+
+    // return idle and total time
+    Ok((idle, total))
+}
