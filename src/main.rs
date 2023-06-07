5c446dc606f3ac9950bdf7578f32bab681432c75
Lennart Heinrich <lennart@ltheinrich.de>
2023-06-07 15:04:37 +0200

    local time + date using chrono

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@
 
 mod sys;
 
+use chrono::{Datelike, Local, Timelike};
 use kern::Fail;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use std::time::Duration;
 use sys::cpu_usage;
 
 fn main() -> Result<(), Fail> {
@@ -20,13 +21,16 @@ fn main() -> Result<(), Fail> {
     let (rc, _) = cpu_usage(Duration::from_secs(60));
     loop {
         let perc = rc.recv().unwrap();
-        let time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+        let time = Local::now();
         log.write_all(
             format!(
-                "{:02}:{:02}:{:02} -> CPU usage ={:>6}%\n",
-                (time.as_secs() / (60 * 60)) % 24,
-                (time.as_secs() / 60) % 60,
-                time.as_secs() % 60,
+                "{:02}.{:02}.{:04} - {:02}:{:02}:{:02} -> CPU usage ={:>6}%\n",
+                time.day(),
+                time.month(),
+                time.year(),
+                time.hour(),
+                time.minute(),
+                time.second(),
                 format!("{:.2}", perc)
             )
             .as_bytes(),
